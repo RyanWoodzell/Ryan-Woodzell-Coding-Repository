1a290620a0a4986f2cecafb6371337a8242a6ab0
Ryan Woodzell <ryanwoodzell@gmail.com>
2024-09-11 18:56:46 -0400

    Add files via upload
    Simple ATM demonstrating python Basics

---
+++ ATMSimple.py
@@ -0,0 +1,130 @@
+
+#Simple ATM
+balance = 10000
+
+def ValidateMenuLimit(value):
+    value = int(value)
+    if(0 < value and value < 4):
+        return True    
+    return False   
+
+def ValidateInteger(value):
+    try:
+        value = int(value)
+        return True
+    except:
+        return False
+
+def ValidateFloat(value):
+    try:
+        value = int(value)
+        return True
+    except:
+        return False 
+
+#   Create a function that validates the user input and user input must be in number range (between 1 and 5)
+def ValidateMenuInput(choice):
+    #   Input must be an integer
+    if (ValidateInteger(choice) and ValidateMenuLimit(choice)):
+        return True
+    return False
+
+#   ********** Allow the user to select what function they'd like to perform ********** 
+def AtmMenu(): 
+    print()
+    print("-" * 60)
+    print()
+    print("Welcome to my ATM machine here are 4 options to choose from:")
+    print()
+    print("-" * 60)
+    print()
+    print("\t1.  Withdrawal")
+    print("\t2.  Credit")
+    print("\t3.  Balance")
+    print("\t4.  Exit program")
+    print()
+    return input("Select from the options above:  ")
+
+#   ********** Display the balance definition **********
+def DisplayBalance(balance):
+    print()
+    print("*" * 7)
+    print("\nThis is your balance: ",balance)
+    print("*" * 7) 
+
+#   **********  Withdraw methods definition **********
+def ValidateWithdrawal(withdrawal):
+    if ValidateFloat(withdrawal):
+        return True
+    else:
+        print("\nPlease enter a dollar amount\n")
+        return False
+
+def HasWithdrawalFunds(withdrawal, balance):
+    if(balance >= withdrawal):
+        return True
+    else:
+        print("Please enter an amount that is within your funds")
+        return False
+
+#######################    
+def Withdrawal(balance):    
+    #   Show the user their balance before they withdrawal
+    DisplayBalance(balance)
+
+  
+    withDrawalAmount = int(input("What amount do you want to withdraw?:  "))
+
+    #   Validate that the amount is a float value and don't allow the user to withdraw more money than they have
+    if ValidateWithdrawal(withDrawalAmount) and HasWithdrawalFunds(withDrawalAmount, balance):
+        print("Handle transactions")
+        # Complete the transaction here
+        balance=balance-withDrawalAmount
+        print("You have succesfuly withdrew ", withDrawalAmount, " dollars and have ", balance, " Dollars left")
+        return(balance)
+    else:
+        print("Handle invalid data")
+
+#######################
+#Deposit Method
+def deposit(balance):
+    depositAmount = float(input("What amount do you want to deposit?:   "))
+    if depositAmount<=0:
+        print("Invalid deposit amount. Try again")
+    else:
+        balance = balance+depositAmount
+        print("Your updated balance is: ",balance)
+        return balance
+#   ********** Exit Program definition **********
+def ExitProgram():
+    quit()
+
+
+
+#   **********  Main Program definition ********** 
+if __name__ == "__main__":
+
+    while True:
+        choice = AtmMenu()
+        if ValidateMenuInput(choice):
+            if choice == '2':
+                balance = deposit(balance)
+            if choice == '3':
+                DisplayBalance(balance)
+            if choice == '1':
+                balance = Withdrawal(balance)
+            if choice == '4':
+                ExitProgram()
+            
+
+           
+
+            
+
+          
+
+        else:
+            break      
+
+    print()
+    print("Thank you for using my ATM application!")
